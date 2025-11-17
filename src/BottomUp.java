f40a719ac55f95d233590200e226ec2428606224
YaBoiSheepish <BIESTEEW5161@uwec.edu>
2025-11-17 13:50:49 -0600

    merge

--- src/BottomUp.java
+++ src/BottomUp.java
@@ -24,7 +24,7 @@ public class BottomUp {
                     right = n;
                 }
 
-                /* merge (A, j, mid, right); */
+                merge (A, j, mid, right);
 
                 j = j + 2 * i;
             }
@@ -32,5 +32,30 @@ public class BottomUp {
             i = i * 2;
 
         }
+    }
+
+    static void merge(int[] A, int start, int mid, int end) {
+        //checking length
+        int left = mid - start;
+        int right = end - mid;
+        // Create temporary arrays based on size of left and right
+        int[] L = new int[left];
+        int[] R = new int[right];
+        // Copy data from A into temporary arrays
+        for (int i = 0; i < left; i++) L[i] = A[start + i];
+        for (int i = 0; i < right; i++) R[i] = A[mid + i];
+
+        int i = 0;
+        int j = 0;
+        int k = start;
+        // Merge elements from L and R back into A in sorted order
+        while (i < left && j < right) {
+            if (L[i] <= R[j]) A[k++] = L[i++];  // if smaller take number
+            else A[k++] = R[j++];
+        }
+
+        // clean up remaining elements
+        while (i < left) A[k++] = L[i++];
+        while (j < right) A[k++] = R[j++];
     }
 }
